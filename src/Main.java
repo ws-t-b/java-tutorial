f2e8ecd8c89c47179702d56a2c3b4e1ea0072543
test <mail@mail.com>
2026-05-14 17:51:16 +0900

    branch test

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,7 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Files.writeString(
-                    Path.of("test.txt"),
-                    "hello java"
-            );
 
-            String text = Files.readString(
-                    Path.of("test.txt")
-            );
-
-            System.out.println(text);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("branch test");
     }
 }
